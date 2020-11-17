1fd193ecb876a3ebd90b88ac7eee7b54f023cf20
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2020-11-17 16:18:03 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 //Blocks Test
 basic.forever(function () {
-    serial.writeLine("" + (scd30.readCO2()))
-    serial.writeLine("" + (scd30.readTemperature()))
-    serial.writeLine("" + (scd30.readHumidity()))
+    serial.writeLine("" + (SCD30.readCO2()))
+    serial.writeLine("" + (SCD30.readTemperature()))
+    serial.writeLine("" + (SCD30.readHumidity()))
     basic.pause(1000)
 })
